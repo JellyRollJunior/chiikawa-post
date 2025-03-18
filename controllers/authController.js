0e6964d9b5f8fb234d8c34cade08ef6544d4d0a2
Brandon Lin <brandonlin9716@gmail.com>
2025-03-18 13:23:28 -0700

    Implement post /auth/sign-up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,16 @@
+import * as db from '../db/queries.js';
+
 const getSignUp = (req, res) => {
     res.render('auth/signUpForm');
 }
 
-export { getSignUp }
+const postSignUp = async (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const username = req.body.username;
+    const password = req.body.password;
+    await db.insertuser(firstname, lastname, username, password);
+    res.redirect('/');
+}
+
+export { getSignUp, postSignUp }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,5 +3,6 @@ import * as authController from '../controllers/authController.js';
 
 const authRouter = Router();
 authRouter.get('/sign-up', authController.getSignUp);
+authRouter.post('/sign-up', authController.postSignUp);
 
 export { authRouter };
