103bbae988bf3be741b002f7663faa262ac3d233
Brandon Lin <brandonlin9716@gmail.com>
2025-03-17 22:40:56 -0700

    Create table schema

---
+++ scripts/relations.sql
@@ -0,0 +1,30 @@
+-- RESET TABLES
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+
+-- CREATE TABLES
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    firstname VARCHAR (32),
+    lastname VARCHAR (32),
+    username VARCHAR (32),
+    password VARCHAR,
+    isAdmin boolean DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR (64),
+    message text,
+    timestamp DATE DEFAULT CURRENT_DATE, 
+    author_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+);
+
+-- INSERT USERS
+INSERT INTO users (firstname, lastname, username, password, isAdmin) VALUES 
+    ('chiikawa', 'chiikawa', 'chiikawa', 'chiikawa', TRUE);
+
+-- INSERT MESSAGES
+INSERT INTO messages (title, message, author_id) VALUES 
+    ('I love to sing!', 'Yan papa rurura rurura!', 1),
+    ('Weeding is difficult work!', 'Nsho! Nsho! Huuuuuu~~~. Waaaaaaaai!', 1);
