6df3dd3d20fabb6451a93a16d69d6c9eb4090609
Brandon Lin <brandonlin9716@gmail.com>
2025-03-24 18:49:00 -0700

    Select trio post companion by default

--- public/scripts/postForm.js
+++ public/scripts/postForm.js
@@ -1,4 +1,11 @@
 const addSelectedImageIcon = () => {
+    const createUsagiIcon = () => {
+        const usagi = document.createElement('img');
+        usagi.classList.add('selected-icon');
+        usagi.src = '/assets/postIcons/usagi-icon.png';
+        return usagi;
+    }
+
     const removeExistingSelectedIcons = () => {
         const icons = document.querySelectorAll('fieldset .selected-icon');
         icons.forEach((icon) => {
@@ -6,17 +13,19 @@ const addSelectedImageIcon = () => {
         });
     };
 
+    // display usagi icon if selected
     const postCompanionFieldset = document.querySelector('fieldset');
     postCompanionFieldset.addEventListener('click', () => {
         removeExistingSelectedIcons();
         const selectedImage = document.querySelector(
             '.image-square:has(input:checked + label)'
         );
-        const selectedIcon = document.createElement('img');
-        selectedIcon.classList.add('selected-icon');
-        selectedIcon.src = '/assets/postIcons/usagi-icon.png';
-        selectedImage.appendChild(selectedIcon);
+        selectedImage.appendChild(createUsagiIcon());
     });
+
+    // select default value
+    const defaultIcon = document.querySelector('.image-square:has(input[id="10"])');
+    defaultIcon.appendChild(createUsagiIcon());
 };
 
 addSelectedImageIcon();
